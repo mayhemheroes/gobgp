cc5e65182eb1fb95c9571c71ff3db7c2ee41882d
Maxwell Koo <30420+mjkoo@users.noreply.github.com>
2025-10-15 15:22:26 +0000

    Nerf asan output

--- rlenv/mayhem/data/scripts/replay.sh
+++ rlenv/mayhem/data/scripts/replay.sh
@@ -7,4 +7,4 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 
-GODEBUG=asyncpreemptoff=1 GOMAXPROCS=1 GOTRACEBACK=crash MAYHEM_TRIAGING=1 /out/fuzz_parse_bgp_message $1
+ASAN_OPTIONS=symbolize=0:print_stacktrace=0 GODEBUG=asyncpreemptoff=1 GOMAXPROCS=1 GOTRACEBACK=crash MAYHEM_TRIAGING=1 /out/fuzz_parse_bgp_message $1
